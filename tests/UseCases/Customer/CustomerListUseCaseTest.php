262187357f0e59986b245882ccec3925c267958c
Rafael Miguel <rafael.lpfrases@gmail.com>
2025-04-21 23:40:19 -0300

    test: adjust tests namespaces

--- tests/UseCases/Customer/CustomerListUseCaseTest.php
+++ tests/UseCases/Customer/CustomerListUseCaseTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\UseCases;
+namespace Tests\UseCases\Customer;
 
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\TestDox;
--- tests/UseCases/User/UserListUseCaseTest.php
+++ tests/UseCases/User/UserListUseCaseTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\UseCases;
+namespace Tests\UseCases\User;
 
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\TestDox;
